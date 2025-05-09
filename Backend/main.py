b986cbfd33f91ac2f479abc0bad434c3c459e67a
n1mply <rudkovskijnikita19@gmail.com>
2025-05-09 21:47:48 +0300

    fixes

--- Backend/main.py
+++ Backend/main.py
@@ -18,7 +18,7 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://192.168.1.10:5173"],
+    allow_origins=["http://localhost:5173", "http://192.168.1.10:5173", "http://87.228.83.115"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Доступ с любого IP в локальной сети
-    port: 5173,      // Порт (можно поменять, если занят)
-    strictPort: true, // Не автоматически искать другой порт, если 5173 занят
-    proxy: {
-      '/api': {
-        target: 'http://192.168.1.10:8000', // Указываем IP ПК
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  
 })
